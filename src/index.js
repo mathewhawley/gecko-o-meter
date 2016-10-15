4ba187b21b381faa43f57f2ac9ac516e91a2690f
Mathew Hawley <mathew@mathewhawley.com>
2016-10-16 00:02:18 +0100

    Connect 'GeckoOMeter' to entry point

---
+++ src/index.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import GeckoOMeter from './components/GeckoOMeter';
+
+ReactDOM.render(<GeckoOMeter />, document.getElementById('root'));
