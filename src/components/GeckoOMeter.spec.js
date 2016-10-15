9841884d6a1d4f5c430005954248eeccfbaa0952
Mathew Hawley <mathew@mathewhawley.com>
2016-10-15 22:21:07 +0100

    Write test – expecting state to be empty on mount

--- src/components/GeckoOMeter.spec.js
+++ src/components/GeckoOMeter.spec.js
@@ -8,4 +8,16 @@ describe('<GeckoOMeter />', () => {
     const wrapper = shallow(<GeckoOMeter />);
     expect(wrapper).to.exist;
   });
+
+  it('should mount with an empty state object', () => {
+    const wrapper = shallow(<GeckoOMeter />);
+
+    expect(
+      Object.keys(wrapper.state()).length
+    ).to.equal(0);
+
+    expect(
+      wrapper.state().constructor === Object
+    ).to.equal(true);
+  });
 });
