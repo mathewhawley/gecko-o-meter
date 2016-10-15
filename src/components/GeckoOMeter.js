bddd133ee85a6b503ba41043e04d733e8e1f78c6
Mathew Hawley <mathew@mathewhawley.com>
2016-10-15 22:25:14 +0100

    Add constructor and set initial state

--- src/components/GeckoOMeter.js
+++ src/components/GeckoOMeter.js
@@ -1,6 +1,11 @@
 import React, { Component } from 'react';
 
 class GeckoOMeter extends Component {
+  constructor() {
+    super();
+    this.state = {};
+  }
+
   render() {
     return <div></div>;
   }
