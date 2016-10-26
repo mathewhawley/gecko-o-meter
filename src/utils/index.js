f31e668229d73554067d2b06987033f790e78731
Mathew Hawley <mathew.hawley@digitaslbi.com>
2016-10-26 13:45:12 +0100

    Export named functions from utils

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,4 +1,4 @@
-export const calculateAngle = (min, max, value) => {
+export function calculateAngle(min, max, value) {
   const result = ((value - min) * 180) / (max - min);
 
   if (result > 180) {
@@ -10,7 +10,7 @@ export const calculateAngle = (min, max, value) => {
   }
 };
 
-export const formatAsCurrency = (num, iso) => {
+export function formatAsCurrency(num, iso) {
   return num.toLocaleString('en', {
     style: 'currency',
     currency: iso,
